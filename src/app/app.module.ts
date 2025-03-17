cd0a90dd2e8e310409e3f81f2b52787d4d547d2d
yanliu1111 <liuyan0504@gmail.com>
2025-03-16 19:07:27 -0600

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RoomsComponent } from './rooms/rooms.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RoomsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
