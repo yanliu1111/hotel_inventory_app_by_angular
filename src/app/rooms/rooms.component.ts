16b37be2c816676f38eb326ea1c91080a012b3da
yanliu1111 <liuyan0504@gmail.com>
2025-03-17 07:30:30 -0600

    feat: learn update

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'app-rooms',
@@ -6,15 +7,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './rooms.component.scss'
 })
 export class RoomsComponent implements OnInit {
-hotelName = 'Hotel California';
-numberOfRooms = 10;
-hideRooms = false;
-  constructor() { }
-// The ngOnInit() method is a lifecycle hook that Angular calls after creating a component.
-// It is a good place to put initialization logic.
-  ngOnInit(): void {
+  hotelName = 'Hotel California';
+  numberOfRooms = 10;
+  hideRooms = false;
+  rooms: Room = {
+    totalRooms: 10,
+    roomsAvailable: 5,
+    bookedRooms: 3,
   }
-  toggle(){
+  roomList: RoomList[] = [
+    {
+      roomNumber: 1,
+      roomType: 'Deluxe Room',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 500,
+      photos:
+        'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime: new Date('20-Nov-2021'),
+
+    },
+    {
+      roomNumber: 2,
+      roomType: 'Deluxe Room',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 1000,
+      photos:
+        'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+      checkinTime: new Date('16-Nov-2021'),
+      checkoutTime: new Date('30-Nov-2021'),
+
+    },
+    {
+      roomNumber: 3,
+      roomType: 'Private Suite',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 15000,
+      photos:
+        'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+      checkinTime: new Date('29-Nov-2021'),
+      checkoutTime: new Date('12-Dec-2021'),
+
+    },
+
+  ];
+
+  constructor() { }
+  // The ngOnInit() method is a lifecycle hook that Angular calls after creating a component.
+  // It is a good place to put initialization logic.
+  ngOnInit(): void { }
+  toggle() {
     this.hideRooms = !this.hideRooms;
   }
 }
---
+++ src/app/rooms/rooms.ts
@@ -0,0 +1,14 @@
+export interface Room {
+  totalRooms: number;
+  roomsAvailable: number;
+  bookedRooms: number;
+}
+export interface RoomList {
+  roomNumber: number;
+  roomType: string;
+  amenities: string;
+  price: number;
+  photos: string;
+  checkinTime: Date;
+  checkoutTime: Date;
+}
