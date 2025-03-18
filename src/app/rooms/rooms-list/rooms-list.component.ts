94b89b7e44a8f0fae412c92ba661ba9c51ea248d
yanliu1111 <liuyan0504@gmail.com>
2025-03-17 19:47:36 -0600

    feat: changeDetection

--- src/app/rooms/rooms-list/rooms-list.component.ts
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -1,11 +1,12 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 import { RoomList } from '../rooms';
 
 @Component({
   selector: 'app-rooms-list',
   templateUrl: './rooms-list.component.html',
-  styleUrl: './rooms-list.component.scss'
+  styleUrl: './rooms-list.component.scss',
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RoomsListComponent implements OnInit {
   @Input() rooms: RoomList[] = [];
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -65,4 +65,17 @@ export class RoomsComponent implements OnInit {
     this.selectedRoom = room;
     console.log('Selected Room:', room);
   }
+  addRoom() {
+    const room: RoomList = {
+      roomNumber: 4,
+      roomType: 'Deluxe Room',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 500,
+      photos: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime: new Date('20-Nov-2021'),
+      rating: 4.5,
+    }
+    this.roomList.push(room);
+  }
 }
