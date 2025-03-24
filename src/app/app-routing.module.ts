2851cc674d53f40cc09c3d5ce7b61d16338c1d91
yanliu1111 <liuyan0504@gmail.com>
2025-03-24 05:34:12 -0600

    feat: add ActivateRoute Service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
 import { NgModule } from '@angular/core';
 import { NotfountComponent } from './notfount/notfount.component';
+import { RoomsBookingComponent } from './rooms/rooms-booking/rooms-booking.component';
 import { RoomsComponent } from './rooms/rooms.component';
 
 const routes: Routes = [
   { path: 'employee', component: EmployeeComponent },
   { path: 'rooms', component: RoomsComponent },
+  { path: 'rooms/:id', component: RoomsBookingComponent },
   { path: '', redirectTo: '/rooms', pathMatch: 'full' },
   { path: '**', component: NotfountComponent }
 ];
--- src/app/rooms/rooms-booking/rooms-booking.component.ts
+++ src/app/rooms/rooms-booking/rooms-booking.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, map } from 'rxjs';
+
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-rooms-booking',
   templateUrl: './rooms-booking.component.html',
   styleUrl: './rooms-booking.component.scss'
 })
-export class RoomsBookingComponent {
+export class RoomsBookingComponent implements OnInit {
+  id: string = '';
+  // id$ = this.router.params.pipe(map(params => params['id']));
+  id$ = this.router.paramMap.pipe(map(params => params.get('id')));
+  constructor(private router: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    // if you use snapshot, you will not get the updated value of the id, will have issue when child component is reloaded
+    // this.id = this.router.snapshot.params['id'];
+    // this.router.params.subscribe(params => {
+    //   this.id = params['id'];
+    // });
+    // this.router.params.subscribe(params => { params.get('id') });
+  }
 
 }
