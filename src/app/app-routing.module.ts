443a400201114d0c517e31e4b7a16093dc71e083
yanliu1111 <liuyan0504@gmail.com>
2025-03-24 06:40:49 -0600

    feat: Rooms-add works, generateUUID done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
 import { NgModule } from '@angular/core';
 import { NotfountComponent } from './notfount/notfount.component';
+import { RoomsAddComponent } from './rooms/rooms-add/rooms-add.component';
 import { RoomsBookingComponent } from './rooms/rooms-booking/rooms-booking.component';
 import { RoomsComponent } from './rooms/rooms.component';
 
 const routes: Routes = [
   { path: 'employee', component: EmployeeComponent },
   { path: 'rooms', component: RoomsComponent },
+  { path: 'rooms/add', component: RoomsAddComponent },
   { path: 'rooms/:id', component: RoomsBookingComponent },
   { path: '', redirectTo: '/rooms', pathMatch: 'full' },
   { path: '**', component: NotfountComponent }
--- src/app/rooms/rooms-add/rooms-add.component.ts
+++ src/app/rooms/rooms-add/rooms-add.component.ts
@@ -1,6 +1,6 @@
-import { FormsModule, NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
-import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { RoomList } from '../rooms';
 import { RoomsService } from '../services/rooms.service';
 
@@ -10,7 +10,7 @@ import { RoomsService } from '../services/rooms.service';
     templateUrl: './rooms-add.component.html',
     styleUrls: ['./rooms-add.component.scss']
   })
-export class RoomsAddComponent {
+export class RoomsAddComponent implements OnInit {
   room: RoomList = {
     roomNumber: '',
     roomType: '',
@@ -27,18 +27,25 @@ export class RoomsAddComponent {
   constructor(private roomsService: RoomsService) { }
 
   AddRoom(roomsForm: NgForm) {
-    this.roomsService.addRoom(this.room).subscribe((data) => {
-      this.successMessage = 'Room added successfully';
-      roomsForm.resetForm({
-        roomNumber: '',
-        roomType: '',
-        amenities: '',
-        price: 0,
-        photos: '',
-        checkinTime: new Date(),
-        checkoutTime: new Date(),
-        rating: 0,
-      });
-    });
+    if (roomsForm.valid) {
+      this.room.roomNumber = this.generateUUID(); // Generate a UUID for roomNumber
+      this.roomsService.addRoom(this.room).subscribe(
+        (data) => {
+          this.successMessage = 'Room added successfully';
+          console.log('Room added:', data);
+        },
+        (error) => {
+          console.error('Error adding room:', error);
+        }
+      );
+      console.log('Form data:', this.room);
+    } else {
+      console.error('Form is invalid');
+    }
   }
+
+  generateUUID(): string {
+    return crypto.randomUUID(); // Use the native crypto API to generate a UUID
+  }
+  ngOnInit(): void { }
 }
