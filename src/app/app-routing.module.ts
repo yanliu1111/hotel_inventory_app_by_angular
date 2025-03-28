ff5c7d3230fd1db709db95858ba40aad0919ad2a
yanliu1111 <liuyan0504@gmail.com>
2025-03-27 22:57:23 -0600

    feat: clean custom decorators

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   {
     path: 'booking', loadChildren: () => import('./booking/booking.module').then(m => m.BookingModule),
-    // canActivate: [loginGuard] 
+    // canActivate: [loginGuard]
   },
   { path: '**', component: NotfountComponent }
 ];
