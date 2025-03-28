d935b7569c4e9941c42ab92f0526274a40fd967f
yanliu1111 <liuyan0504@gmail.com>
2025-03-27 21:26:10 -0600

    feat: valueChange and UpdateOn

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -17,13 +17,13 @@ export class BookingComponent implements OnInit {
   ngOnInit(): void {
     this.bookingForm = this.fb.group({
       roomId: new FormControl({ value: '2', disabled: true }, { validators: [Validators.required] }),
-      guestEmail: ['', { validators: [Validators.required, Validators.email] }],
+      guestEmail: ['', { updateOn: 'blur', validators: [Validators.required, Validators.email] }],
       checkinDate: [''],
       checkoutDate: [''],
       bookingStatus: [''],
       bookingAmount: [''],
       bookingDate: [''],
-      mobileNumber: [''],
+      mobileNumber: ['', { updateOn: 'blur' }],
       guestName: ['', [Validators.required, Validators.minLength(5)]], // Added minlength validator
       address: this.fb.group({
         addressLine1: ['', Validators.required], // Fixed typo: AddresLine1 -> addressLine1
@@ -37,6 +37,10 @@ export class BookingComponent implements OnInit {
         this.addGuestControl()
       ]),
       tnc: new FormControl(false, { validators: [Validators.requiredTrue] }),
+    });
+    this.getBookingData();
+    this.bookingForm.valueChanges.subscribe((data) => {
+      // console.log("data", data);
     });
   }
   addBooking() {
@@ -65,10 +69,34 @@ export class BookingComponent implements OnInit {
       guests: [],
       tnc: false,
     });
+
   }
   // addBooking() {
   //   console.log(this.bookingForm.getRawValue());
   // }
+  getBookingData() {
+    this.bookingForm.patchValue({
+      // roomId: '2',
+      guestEmail: 'test@gmail.com',
+      checkinDate: new Date('27-March-2025'),
+      // checkoutDate: '',
+      bookingStatus: '',
+      bookingAmount: '',
+      bookingDate: '',
+      mobileNumber: '',
+      guestName: '',
+      Address: {
+        addressLine1: '',
+        addressLine2: '',
+        city: '',
+        state: '',
+        country: '',
+        zipCode: '',
+      },
+      guests: [],
+      tnc: false,
+    })
+  }
   addGuest() {
     this.guests.push(
       this.addGuestControl()
@@ -77,7 +105,7 @@ export class BookingComponent implements OnInit {
   addGuestControl() {
     return this.fb.group({
       guestName: ['', Validators.required],
-      age: new FormControl(''),
+      age: new FormControl('', { validators: [Validators.required] }),
     });
   }
   addPassport() {
